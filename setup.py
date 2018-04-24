bf7e5f10ca36b29d59ae857c3d7cf0ea276cf43b
Justin Caratzas <bigjust@lambdaphil.es>
2018-04-23 23:20:03 -0400

    cleaned up setup.py, dropped py26

--- setup.py
+++ setup.py
@@ -3,28 +3,6 @@ import pkg_resources
 
 from setuptools import setup, find_packages
 
-from pip.req import parse_requirements as parse_reqs
-
-
-# Compatibility with older versions of pip
-pip_dist = pkg_resources.get_distribution('pip')
-pip_version = tuple(map(int, pip_dist.version.split('.')))
-
-# Use a base partial that will be updated depending on the version of pip
-parse_requirements = functools.partial(parse_reqs, options=None)
-
-if pip_version < (1, 2):
-    # pip versions before 1.2 require an options keyword for using it outside
-    # of invoking a pip shell command
-    from pip.baseparser import parser
-    parse_requirements.keywords['options'] = parser.parse_args()[0]
-
-if pip_version >= (1, 5):
-    # pip 1.5 introduced a session kwarg that is required in later versions
-    from pip.download import PipSession
-    parse_requirements.keywords['session'] = PipSession()
-
-
 setup(
     name='helga-reminders',
     version='0.1.1',
@@ -37,7 +15,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
@@ -49,7 +26,7 @@ setup(
     py_modules=['helga_reminders'],
     include_package_data=True,
     install_requires=[
-        str(req.req) for req in parse_requirements('requirements.txt')
+        'pytz',
     ],
     zip_safe=True,
     entry_points=dict(
